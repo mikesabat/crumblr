7fa9474176e8031f62b435a12342e8c69c4ff252
Michael <mike.sabat@gmail.com>
2013-10-08 22:46:03 -0400

    ability to add images as posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131006162916) do
+ActiveRecord::Schema.define(:version => 20131009022441) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "kind"
   end
 
 end
